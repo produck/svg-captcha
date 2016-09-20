08acc9203011ca7d0e1ea790acdeb7b708af53e3
Weilin <934587911@qq.com>
2016-09-20 19:26:27 +0800

    enforce stricter code style

--- test/test.js
+++ test/test.js
@@ -10,34 +10,34 @@ const it = mochaSugar.it;
 const xmlReg = /^<svg[\s\S]+\/svg>$/;
 const textTagReg = /text/;
 
-describe('svg captcha', function () {
-	it('should generate random text', function () {
+describe('svg captcha', () => {
+	it('should generate random text', () => {
 		for (let i = 0; i < 62; i++) {
-			let text = svgCaptcha.randomText();
+			const text = svgCaptcha.randomText();
 			assert(/^[0-9a-zA-Z]+$/.test(text));
 		}
 	});
 
-	it('should filter unwanted chars', function () {
+	it('should filter unwanted chars', () => {
 		const opt = {ignoreChars: '0123456789'};
 		for (let i = 0; i < 62; i++) {
-			let text = svgCaptcha.randomText(opt);
+			const text = svgCaptcha.randomText(opt);
 			assert(/^[a-zA-Z]+$/.test(text));
 		}
 	});
 
-	it('should generate svg', function () {
+	it('should generate svg', () => {
 		assert(xmlReg.test(svgCaptcha()));
 		assert(xmlReg.test(svgCaptcha({text: 'abcd'})));
 	});
 
-	it('should generate path', function () {
+	it('should generate path', () => {
 		assert(!textTagReg.test(svgCaptcha({text: 'text'})));
 	});
 
-	it('should be fast', function () {
+	it('should be fast', () => {
 		for (let i = 0; i < 100; i++) {
-			let text = svgCaptcha.randomText();
+			const text = svgCaptcha.randomText();
 			svgCaptcha(text);
 		}
 	}, {
@@ -48,15 +48,15 @@ describe('svg captcha', function () {
 
 const random = require('../lib/random');
 
-describe('random function', function () {
-	it('should generate random integer', function () {
-		for (var i = 0; i < 10; i++) {
-			var num = random.int(0, 10);
+describe('random function', () => {
+	it('should generate random integer', () => {
+		for (let i = 0; i < 10; i++) {
+			const num = random.int(0, 10);
 			assert(num >= 0 && num <= 10);
 		}
 	});
 
-	it('should generate grey color', function () {
+	it('should generate grey color', () => {
 		assert(random.greyColor());
 		assert(random.greyColor(3, 4));
 	});
--- test/visual.js
+++ test/visual.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const svg = require('../');
 
 for (let i = 0; i < 10; i++) {
-	fs.writeFile(`test${i}.svg`, svg(svg.randomText()), 'utf8', function (err) {
+	fs.writeFile(`test${i}.svg`, svg(svg.randomText()), 'utf8', err => {
 		if (err) {
 			console.error(err);
 		} else {
