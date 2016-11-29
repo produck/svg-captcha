5bd828cc22a041302a3a8d4ebf03d29a9c144b0c
Weilin <934587911@qq.com>
2016-11-29 10:57:54 +0800

    [UPDATED] typescript definition [REMOVED] translate attribute

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,17 @@
 /**
- * 
+ * This main exported function takes a string as input
+ * and returns a svg image
  */
 export function createCaptcha(text: string): string;
 export namespace createCaptcha {
 	/**
-	 * 
+	 * generate random text of length 4
 	 */
 	export function randomText(size: number): string;
 	/**
-	 * 
+	 * This method returns a object that has two props:
+	 * data: svg image
+	 * text: captcha text
 	 */
 	export function create(options: Object): Object;
 }
