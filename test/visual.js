ba2f4fdb24d34fac260e17f499a2adac610e8461
Weilin <934587911@qq.com>
2017-03-30 17:22:59 +0800

    [FIXED] how characters are align in image [UPDATED] dependencies

--- test/visual.js
+++ test/visual.js
@@ -32,3 +32,17 @@ fs.writeFile(`${TEST_DIR}/20.svg`, svg.create({inverse: true}).data, 'utf8', err
 		console.log('it\'s saved');
 	}
 });
+
+fs.writeFile(
+	`${TEST_DIR}/21.svg`,
+	svg.create({width: 40, height: 18, fontSize: 24}).data,
+	saveCb
+);
+
+function saveCb(err) {
+	if (err) {
+		console.error(err);
+	} else {
+		console.log('it\'s saved');
+	}
+}
