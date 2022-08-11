b04fe7f6d411ebe164544f2e8d74fc71f199dae4
ecodemo <ecodemo@163.com>
2022-08-11 16:08:48 +0800

    Fix: fonts not found after packed

--- index.d.ts
+++ index.d.ts
@@ -67,6 +67,10 @@ declare class ConfigObject {
 	 * max value of the math expression
 	 */
 	mathMax?: number;
+	/**
+	 * default: path.join(__dirname, "../fonts/Comismsh.ttf");
+	 */
+	font?: string;
 }
 /**
  * result of captcha generation
